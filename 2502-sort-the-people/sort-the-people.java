534b3f8acfdee0b2fb18f6d259db992dfe6193aa
Gopal Choudhary <116931814+gopallchoudhary@users.noreply.github.com>
2026-01-03 22:22:22 +0530

    Time: 11 ms (47.17%) | Memory: 47.8 MB (9.74%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,32 +1,33 @@
 class Solution {
-    class Person implements Comparable<Person> {
-        String name;
-        int height;
+    // class Person implements Comparable<Person> {
+    //     String name;
+    //     int height;
 
-        Person(String n, int h) {
-            this.name = n;
-            this.height = h;
-        }
+    //     Person(String n, int h) {
+    //         this.name = n;
+    //         this.height = h;
+    //     }
 
-        public int compareTo(Person P2) {
-            return P2.height - this.height;
-        }
-    }
+    //     public int compareTo(Person P2) {
+    //         return P2.height - this.height;
+    //     }
+    // }
     public String[] sortPeople(String[] names, int[] heights) {
-        int n = names.length;
-        Person p[] = new Person[n];
+       int n = names.length;
+       Map<Integer, String> heightToNameMap = new HashMap<>();
 
-        for(int i=0; i<n; i++) {
-            p[i] = new Person(names[i], heights[i]);
-        }
+       for(int i=0; i<n; i++) {
+        heightToNameMap.put(heights[i], names[i]);
+       }
 
-        Arrays.sort(p);
-        String result[] = new String[n];
+       Arrays.sort(heights);
 
-        for(int i=0; i<n; i++) {
-            result[i] = p[i].name;
-        }
+       String result[] = new String[n];
 
-        return result;
+       for(int i=n-1; i>=0; i--) {
+        result[n-i-1] = heightToNameMap.get(heights[i]);
+       }
+
+       return result;
     }
 }
