3936f83efa2487d20efff972510c2f7a07e66245
Gopal Choudhary <116931814+gopallchoudhary@users.noreply.github.com>
2026-01-09 22:23:11 +0530

    Time: 31 ms (54.54%) | Memory: 48.5 MB (71.37%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> set = new HashSet<>();
+        for(int c: candyType) {
+            set.add(c);
+        }
+
+        int n = (candyType.length)/2;
+        int m = set.size();
+        if(n < m) {
+            return n;
+        } else {
+            return m;
+        }
+     }
+}
