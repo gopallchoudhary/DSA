82c8fe763f3ff5687e62bea3668dbf062aee9379
Gopal Choudhary <116931814+gopallchoudhary@users.noreply.github.com>
2026-01-16 08:42:12 +0530

    Time: 2 ms (98.84%) | Memory: 43.9 MB (98.42%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.isEmpty()) {
+            return true;
+        }
+        int first = 0;
+        int last = s.length()-1;
+        while(first<=last) {
+            char firstChar = s.charAt(first);
+            char lastChar = s.charAt(last);
+
+            //check for the special character or digit
+            if(!Character.isLetterOrDigit(firstChar)) {
+                first++;
+            }
+
+            else if(!Character.isLetterOrDigit(lastChar)) {
+                last--;
+            }
+
+            else {
+                if(Character.toLowerCase(firstChar) != Character.toLowerCase(lastChar)) {
+                return false;
+                }
+                first++;
+                last--;
+            }
+        }
+        return true;
+    }
+}
