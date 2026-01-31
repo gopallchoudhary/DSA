c857941b8080c708182e017a1a18f68b731b0ebc
Gopal Choudhary <116931814+gopallchoudhary@users.noreply.github.com>
2026-01-31 17:39:22 +0530

    Time: 9 ms (95.18%) | Memory: 46.9 MB (77.53%) - LeetSync

--- 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.java
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.java
@@ -6,14 +6,23 @@ class Solution {
 
         for (int[] box : boxTypes) {
 
-            if (truckSize == 0) {
+            // if (truckSize == 0) {
+            //     break;
+            // }
+
+            // int takeBoxes = truckSize < box[0] ? truckSize : box[0];
+
+            // units += takeBoxes * box[1];
+            // truckSize -= takeBoxes;
+            if(truckSize >= box[0]) {
+                units+=box[0] * box[1];
+                truckSize-=box[0];
+            } else {
+                units+=truckSize*box[1];
+                truckSize = 0;
                 break;
             }
-
-            int takeBoxes = truckSize < box[0] ? truckSize : box[0];
-
-            units += takeBoxes * box[1];
-            truckSize -= takeBoxes;
+           
         }
 
         return units;
